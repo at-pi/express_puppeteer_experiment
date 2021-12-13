315ee73485790a79c99cf1b41fe30ef0d025fdda
Aditya Tiwari <aditya.tiwari@tact.ai>
2021-12-14 00:35:43 +0530

    corrected api for url oprning

--- routes/urls.js
+++ routes/urls.js
@@ -14,11 +14,38 @@ const func = async (url) => {
     return page
 }
 
-router.get('/url', (req, res) => {
+router.get('/url', (req,res) => {
     
-    const id = req.query.id
     const url = req.query.url
-    
+
+    const page = new Promise((resolve, reject) => {
+        func(url)
+        .then( page => {
+            console.log('successfully opened '+url)
+            resolve(page)
+        })
+        .catch( err => reject('failed in opening url'))
+    })
+
+    Promise.all([page])
+    .then( page => {
+        let obj = {
+            'id' : i++,
+            'browser' : page
+        }
+        storage.push(obj)
+        res.send('opened browser success, id:'+ i)
+    })
+
 })
+
+// router.get('/url', (req, res) => {
+    
+//     const id = req.query.id
+//     const url = req.query.url
+    
+//     console.log('Hi')
+//     res.send('hi')
+// })
 
 module.exports = router
