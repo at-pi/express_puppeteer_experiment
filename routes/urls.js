0c53f014d50631bf2012b07a1fab6e0e84e060c2
Aditya Tiwari <aditya.tiwari@tact.ai>
2021-12-14 01:24:44 +0530

    second api

--- routes/urls.js
+++ routes/urls.js
@@ -14,38 +14,70 @@ const func = async (url) => {
     return page
 }
 
+const func2 = async (page, url) => {
+    const browser = await puppeteer.launch()
+    page = await browser.newPage()
+    await page.goto(url)
+    
+    return page
+}
+
 router.get('/url', (req,res) => {
     
     const url = req.query.url
+    const id = req.query.id
 
-    const page = new Promise((resolve, reject) => {
-        func(url)
-        .then( page => {
-            console.log('successfully opened '+url)
-            resolve(page)
+    if( id == null){
+
+        const page = new Promise((resolve, reject) => {
+            func(url)
+            .then( page => {
+                console.log('successfully opened '+url)
+                resolve(page)
+            })
+            .catch( err => reject('failed in opening url'))
         })
-        .catch( err => reject('failed in opening url'))
-    })
+    
+        Promise.all([page])
+        .then( page => {
+            let obj = {
+                'id' : i++,
+                'browser' : page
+            }
+            storage.push(obj)
+            res.send('opened browser success, id:'+ i)
+        })
+    }
+    else{
+        console.log(storage)
 
-    Promise.all([page])
-    .then( page => {
-        let obj = {
-            'id' : i++,
-            'browser' : page
-        }
-        storage.push(obj)
-        res.send('opened browser success, id:'+ i)
-    })
+        storage.forEach( function(item) {
+
+            console.log(item.id)
+            console.log(item.browser)
+
+            if( id == item.id){
+
+                item.browser = new Promise( (resolve, reject) => {
+                    func2(item.browser, url)
+                    .then( page => {
+                        console.log('successfully shifted the page')
+                        resolve(page)
+                    })
+                    .catch( err => reject('could not shift the page'))
+                })
+
+                Promise.all([item.browser])
+                .then( page => {
+                    res.send(item.id+' '+item.browser)
+                    console.log(item.browser)
+                })
+                
+                return false
+            }
+        })
+    }
 
 })
-
-// router.get('/url', (req, res) => {
-    
-//     const id = req.query.id
-//     const url = req.query.url
-    
-//     console.log('Hi')
-//     res.send('hi')
-// })
 
 module.exports = router
